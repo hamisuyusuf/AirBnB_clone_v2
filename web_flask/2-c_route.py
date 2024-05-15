26f62bc82aa3aee1516190b6630ab57b9e4abb80
hamisuyusuf <hamisuyusuf180@gmail.com>
2024-05-15 06:34:48 -0700

    Task4

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -24,4 +24,4 @@ def Display_text(text):
     return "C {}".format(text.replace("-", " "))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
